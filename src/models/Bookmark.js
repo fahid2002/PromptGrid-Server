e34f7ed945e3a186218a039cf191a9b5ebcce63a
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-22 03:59:57 +0600

    bookmark

--- src/models/Bookmark.js
+++ src/models/Bookmark.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
-const schema = new mongoose.Schema({ user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, prompt: { type: mongoose.Schema.Types.ObjectId, ref: 'Prompt', required: true } }, { timestamps: true });
+const schema = new mongoose.Schema(
+    {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+        prompt: { type: mongoose.Schema.Types.ObjectId, ref: 'Prompt', required: true } }, { timestamps: true });
 schema.index({ user: 1, prompt: 1 }, { unique: true });
 export default mongoose.models.Bookmark || mongoose.model('Bookmark', schema);
