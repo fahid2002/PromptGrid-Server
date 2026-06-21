262e28d0dcd0e8495c346961bf7a4165b6aa5c91
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-22 03:53:35 +0600

    uploadcontroller

--- src/controllers/upload-controller.js
+++ src/controllers/upload-controller.js
@@ -1,5 +1,8 @@
 import { storeImage } from '../services/gridfs-image.js';
 
+// Uploads image file and stores it using GridFS image service
 export async function upload(request, response) {
-  response.status(201).json(await storeImage(request.file));
+  response
+    .status(201)
+    .json(await storeImage(request.file));
 }
