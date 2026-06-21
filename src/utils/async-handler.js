9a979577f3d133717739de072e9e68e155ead3f9
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-22 04:34:23 +0600

    async

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -1,1 +1,5 @@
-export const asyncHandler = (handler) => (request, response, next) => Promise.resolve(handler(request, response, next)).catch(next);
+// Wraps async route handlers and sends errors to Express error handler
+export const asyncHandler = (handler) => (request, response, next) =>
+  Promise
+    .resolve(handler(request, response, next))
+    .catch(next);
