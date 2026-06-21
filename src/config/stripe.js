20538c12a13f88667b46e1dd7c329d1225e75a16
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-22 03:32:28 +0600

    stripe

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,3 +1,7 @@
 import Stripe from 'stripe';
 import { env } from './env.js';
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, { apiVersion: '2026-02-25.clover' });
+
+// Create Stripe instance using secret key from environment variables
+export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+  apiVersion: '2026-02-25.clover',
+});
