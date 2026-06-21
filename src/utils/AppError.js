677e5a30f076ada3a0dd56d4e2c8ff71ac7a40c7
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-22 04:33:43 +0600

    apperror

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -1,3 +1,9 @@
+// Custom error class for API errors
 export class AppError extends Error {
-  constructor(status, message, details) { super(message); this.status = status; this.details = details; }
+  constructor(status, message, details) {
+    super(message);
+
+    this.status = status;
+    this.details = details;
+  }
 }
