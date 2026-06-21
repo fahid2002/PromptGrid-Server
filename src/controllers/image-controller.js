c631421305c15ad862352a9d0ee646bff0a53a43
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-22 03:40:09 +0600

    imagecontroller

--- src/controllers/image-controller.js
+++ src/controllers/image-controller.js
@@ -1,7 +1,20 @@
-import { openImageStream, readImageMetadata } from '../services/gridfs-image.js';
+import {
+  openImageStream,
+  readImageMetadata,
+} from '../services/gridfs-image.js';
 
 export async function showImage(request, response) {
+  // Read image metadata from GridFS using image id
   const { contentType } = await readImageMetadata(request.params.id);
-  response.set({ 'Content-Type': contentType, 'Cache-Control': 'public, max-age=31536000, immutable' });
-  openImageStream(request.params.id).on('error', () => response.destroy()).pipe(response);
+
+  // Set image response headers
+  response.set({
+    'Content-Type': contentType,
+    'Cache-Control': 'public, max-age=31536000, immutable',
+  });
+
+  // Open image stream and send it to the response
+  openImageStream(request.params.id)
+    .on('error', () => response.destroy())
+    .pipe(response);
 }
